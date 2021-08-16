941a5fe33dd5b66ad57c1fc5346a1e0c5bd9ef91
Vladyslav <kupriianovvladyslav@gmail.com>
2021-08-16 23:49:38 +0300

    json to index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import allCars from './all-cars.json';
 import Auto from './components/Auto/Auto'
 import {BrowserRouter, Route} from "react-router-dom";
 import AutoDetails from "./components/AutoDetails/AutoDetails";
@@ -8,8 +7,8 @@ const App = (props) => {
     return (
         <BrowserRouter>
             <div className="App">
-                <Route exact path='/main' render={ () => <Auto cars={allCars}/> } />
-                <Route exact path='/details/:id' render={ () => <AutoDetails cars={allCars}/> } />
+                <Route exact path='/main' render={ () => <Auto cars={props.cars}/> } />
+                <Route exact path='/details/:id' render={ () => <AutoDetails cars={props.cars}/> } />
             </div>
         </BrowserRouter>
     );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import allCars from './all-cars.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App cars={allCars}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
